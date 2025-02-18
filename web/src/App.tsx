548f8a2439382922e7158cc19889034df3842dd1
Devin Lim <davay@devinl.im>
2025-02-18 01:01:26 -0800

    init web

---
+++ web/src/App.tsx
@@ -0,0 +1,8 @@
+import Chatbot from '@/components/Chatbot';
+import "./App.css";
+
+export default function App() {
+  return (
+    <Chatbot />
+  );
+}
---
+++ web/src/components/Chatbot.tsx
@@ -0,0 +1,7 @@
+export default function Chatbot() {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
+}
