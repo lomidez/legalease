250b33fb0d4d8b71d89e6d1262bf0bcbd3341b43
Nicholas McNamara <nmcnamara@seattleu.edu>
2025-02-21 21:34:24 -0800

    Updated functions and prompts

--- LegalEaseMk1/converse.py
+++ LegalEaseMk1/converse.py
@@ -1,11 +1,11 @@
 # Imports to work in a nonrunning environment:
 from chat_history import get_chat_history, save_chat_history, get_summary, save_summary
 from rag import query_rag
-print('here')
-from model import generate_response, init_model
+from model_llama2_7b import generate_response, init_model
+#from model import generate_response, init_model
 import warnings
 import sys
-sys.stderr = open('/dev/null', 'w')
+#sys.stderr = open('/dev/null', 'w')
 
 
 warnings.filterwarnings("ignore")
@@ -19,12 +19,14 @@ def get_prompt(file_name):
     return prompt
 
 def main():
+    
     #get summary_prompt from summary_prompt.txt
     summary_prompt = get_prompt('summary_prompt.txt')
     #get class_prompt form class_prompt.txt
     class_prompt = get_prompt('class_prompt.txt')
     prompt = get_prompt('prompt.txt')
     model, tokenizer = init_model()
+    print(generate_response('I want to make a business caled legalEase that classifies business structures using AI', model, tokenizer))
 
     #####################################################################################
     #Summarization Section:
@@ -32,41 +34,51 @@ def main():
     print("To get us started, please tell me everything you know about your business so far:")
     still_summarizing = True
     # flag to allow the user to stop summarizing. If they havent said anything, we can't move on.
-    first_pass = False
+    after_first_pass = False
     while still_summarizing:
-        user_input = input("'quit' to exit: ")
-        if user_input == 'quit' and first_pass:
+        if after_first_pass:
+            user_input = input("To move on, type 'next' ")
+        else:
+            user_input = input("Ideas go here => ")
+        if user_input == 'next' and after_first_pass:
             still_summarizing = False
-        elif user_input == '':
-            print("Please enter a valid question.")
+        elif len(user_input) < 15:
+            print("We can't do the thinking for you, come back with a plan.")
         else:
             # No Rag used in summarization stage
             summary = get_summary()
-            print('sum grabbed')
             complete_query = summary_prompt.format(summary = summary, user_input=user_input)
-            print('q made')
-            llm_output = generate_response(model, tokenizer, complete_query)
-            print('llm r')
-            
+            llm_output = generate_response(complete_query, model, tokenizer)
             # how to clean text of everything but letters and spaces
             print("LegalEase:", llm_output)        
             save_summary(llm_output)
         # Allow a user to move on
-        first_pass = True
+        after_first_pass = True
 
 
     ############################################################################################
     # Classification Section:
-    print('We will now summarize your ideas and suggest a business structure. This may take a few moments...')
+    print('\nWe will now summarize your ideas and suggest a business structure. This may take a few moments...')
     summary = get_summary()
-    # format prompt
+    # Grab RAG and format prompt
+    if len(summary) == 0:
+        print('No information about your business on file. Please Try again.')
+        exit(0)
+    rag_context= 'query_rag(summary)'
     complete_query = class_prompt.format(summary = summary, rag_context=rag_context)
-    class_output = generate_response(model, tokenizer, complete_query)
+    class_output = generate_response(complete_query, model, tokenizer)
     # Save this decision to the business summary:
     save_summary("The best business structure for this situation is" + class_output)
     print(class_output)
-    print('this is your business summary:')
+    print('this is what we have so far concering your business:')
     print(get_summary())
+
+    print('do you have any other questions at this time?')
+    ##TODO ADD prompt section 
+
+    ##TODO EXTRACT LLM to get parts of ARTICES of INCORPORATION
+
+    ##TODO we have to manually aprse that beast
 
 
 main()
